3eea2df32cd82fa1709600316cba5743556c1548
LinDu <lindu0515@gmail.com>
2015-09-07 21:44:34 -0400

    09/07/2015 Update Android Interview App

---
+++ CrackingtheInterview/CrackingtheInterview/app/src/main/java/com/lindu/crackingtheinterview/FrontPage.java
@@ -0,0 +1,16 @@
+package com.lindu.crackingtheinterview;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class FrontPage extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front_page);
+    }
+}
